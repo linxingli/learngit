3bacff11996c19568d530c9f80dff3ba5797d211
linxingli <1531780174@qq.com>
2022-10-22 17:41:47 +0800

    test node

--- jugeChangePath.js
+++ jugeChangePath.js
@@ -1,4 +1,4 @@
 
-// const execSync = require('child_process').execSync; //同步子进程
-// let status = execSync('git status').toString().trim();
-// console.log('status', status)
+const execSync = require('child_process').execSync; //同步子进程
+let status = execSync('git status').toString().trim();
+console.log('status', status)
