90b0b7ea8a957b20a53a7c61710c67a0361ec720
linxingli <1531780174@qq.com>
2022-10-22 18:20:03 +0800

    test node

--- jugeChangePath.js
+++ jugeChangePath.js
@@ -1,4 +1,9 @@
 
 const execSync = require('child_process').execSync; //同步子进程
-let status = execSync('git status').toString().trim();
-console.log('status', status)
+let statusStr = execSync('git status').toString().trim();
+
+// 获取增删改的文件路径数组
+let arr = statusStr.match(/(new file|deleted|modified): (.*)/g)
+let changePathArr = arr.map(v => v.split(':')).map(v => v[1].trim())
+
+console.log('changePathArr', changePathArr)
